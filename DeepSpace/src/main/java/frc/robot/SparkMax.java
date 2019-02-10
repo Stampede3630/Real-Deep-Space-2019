ad248dd09713a2e9999109c206d0199166f63219
camclarke64 <camsyta81600@gmail.com>
2019-02-10 17:01:53 -0600

    This is a basic outline for our SparkMax motor. We can change later.

--- DeepSpace/src/main/java/frc/robot/SparkMax.java
+++ DeepSpace/src/main/java/frc/robot/SparkMax.java
@@ -1,5 +1,36 @@
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class SparkMax {
-    
+
+    XboxController xBox;
+
+    CANSparkMax motor;
+    CANEncoder encoder;
+
+    public static final int deviceID = 1;
+
+    public SparkMax() {
+        xBox = new XboxController(0);
+        motor = new CANSparkMax(deviceID, MotorType.kBrushless);
+        encoder = motor.getEncoder();
+    }
+
+    //we want to use radians per second for our calculations
+    public double rpmToRadiansPerSecond(double rpm) {
+        rpm = rpm * ((Math.PI) / 30);
+        return rpm;
+    }
+
+    //basic outline for SDB code
+    public void teleopPeriodic() {
+        motor.set(xBox.getY());
+        SmartDashboard.putNumber("Motor speed", rpmToRadiansPerSecond(encoder.getVelocity()));
+    }
 }
