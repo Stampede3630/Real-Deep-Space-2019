2e276acf8a2f344e65ea2adf53dcdcacc9254499
Sam Detpr <detosa@student.breckschool.org>
2019-02-13 09:37:32 -0600

    Create GrassHopper.java

---
+++ DeepSpace/src/main/java/frc/robot/GrassHopper.java
@@ -0,0 +1,61 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Timer;
+
+public class GrassHopper {
+    // Create and initialize 4 double soleniods, 2 things that slide the solenionds, and a Timer 
+    //Front refers to the front 2 legs
+    //back refers to the back 2 legs
+    // hopUp is the execute method
+    GrassHopper()
+    {
+
+    }
+
+    public void frontExtend()
+    {
+
+    }
+
+    public void frontRetract()
+    {
+
+    }
+
+    public void backExtend()
+    {
+
+    }
+
+    public void backRetract()
+    {
+
+    }
+
+    public void backLegsSlideForward()
+    {
+
+    }
+
+    public void backLegsSlideBack()
+    {
+
+    }
+
+    public void frontLegsSlideForward()
+    {
+
+    }
+
+    public void frontLegsSlideBack()
+    {
+
+    }
+
+    public void hopUp ()
+    {
+
+    }
+
+}
