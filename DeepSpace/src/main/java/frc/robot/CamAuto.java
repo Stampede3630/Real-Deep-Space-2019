e49ffe35720151c1a45b922c4fc7f2b126da3856
camclarke64 <camsyta81600@gmail.com>
2019-02-14 17:43:03 -0600

    We actually fixed our robot timer code!

--- DeepSpace/src/main/java/frc/robot/CamAuto.java
+++ DeepSpace/src/main/java/frc/robot/CamAuto.java
@@ -1,15 +1,20 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class CamAuto {
 
     RobotMap robotMap = RobotMap.getRobotMap();
-    Timer timer = new Timer();
-    
-    public void camTestMethod() {
+    Timer timer;
 
+    public CamAuto() {
+        timer = new Timer();
         timer.start();
+    }
+    
+    public void camTestMethod() {        
+        SmartDashboard.putNumber("Timer Value", timer.get());
         if (timer.get() > 2) {
             robotMap.drive.driveCartesian(0, 0, 0);
         }
