64519a0b5aec630ae36771075d69040894dff189
camclarke64 <camsyta81600@gmail.com>
2019-02-13 18:45:00 -0600

    Added stop code with timerr

--- DeepSpace/src/main/java/frc/robot/CamAuto.java
+++ DeepSpace/src/main/java/frc/robot/CamAuto.java
@@ -1,10 +1,19 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class CamAuto {
 
     RobotMap robotMap = RobotMap.getRobotMap();
+    Timer timer = new Timer();
 
     public void stop() {
-        
+        if (timer.get() < 2) {
+            robotMap.drive.driveCartesian(0, 0.4, 0);
+        }
+
+        else if (timer.get() > 2) {
+            robotMap.drive.driveCartesian(0, 0, 0);
+        }
     }
 }
