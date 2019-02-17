71e94c224ea167eaf44a7200472142b654d4d227
camclarke64 <camsyta81600@gmail.com>
2019-02-16 22:28:37 -0600

    Moved code to proper branch

--- DeepSpace/src/main/java/frc/robot/CamAuto.java
+++ DeepSpace/src/main/java/frc/robot/CamAuto.java
@@ -4,28 +4,24 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class CamAuto {
-
-    RobotMap robotMap = RobotMap.getRobotMap();
-    Timer timer;
     NavX navx = new NavX();
 
     public CamAuto() {
-        timer = new Timer();
-        timer.start();
+        
     }
     
     public void camTestMethod() {
-        SmartDashboard.putNumber("Timer Value", timer.get());
+        /*SmartDashboard.putNumber("Timer Value", timer.get());
 
-        if (timer.get() < 4 && !navx.collisionDetector()) {
+        if (timer.get() < 4) {
             robotMap.drive.driveCartesian(0, 0.4, 0);
         }
         
         else {
-            robotMap.drive.driveCartesian(0, 0, 0);
+            //robotMap.drive.driveCartesian(0, 0, 0);
             //for ball deploy auto to work isLaunch has to be true
             //Robot.manipulator.manipulatorMode.deployAuto(false);
-        }
+        }*/
 
         navx.accelerationDiagnostics();
     }
--- DeepSpace/src/main/java/frc/robot/NavX.java
+++ DeepSpace/src/main/java/frc/robot/NavX.java
@@ -9,54 +9,22 @@ public class NavX {
 
     AHRS navx;
     
-    boolean collisionDetected = false;
+    double alphaX;
+    double alphaY;
+    final double time = 0.02;
     
-    double lastWorldLinearAccelX = 0;
-    double lastWorldLinearAccelY = 0;
-    double currentWorldLinearAccelX = 0;
-    double currentWorldLinearAccelY = 0;
-    double currentJerkX = 0;
-    double currentJerkY = 0;
-    double jerkThreshhold = 0.5; //inches per second cubed
-
     public NavX() {
         navx = new AHRS(SPI.Port.kMXP);
     }
 
-    public boolean collisionDetector() {
-        //robotMap.drive.driveCartesian(0, -0.4, 0);
-        
-        currentWorldLinearAccelX = navx.getWorldLinearAccelX();
-        currentJerkX = currentWorldLinearAccelX - lastWorldLinearAccelX;
-        lastWorldLinearAccelX = currentWorldLinearAccelX;
-        
-        currentWorldLinearAccelY = navx.getWorldLinearAccelY();
-        currentJerkY = currentWorldLinearAccelY - lastWorldLinearAccelY;
-        lastWorldLinearAccelY = currentWorldLinearAccelY;
-
-        if ((Math.abs(currentJerkY) > jerkThreshhold)) {
-            collisionDetected = true;
-            //robotMap.drive.driveCartesian(0, 0, 0);
-            System.out.print("Crash boom :)");
-        }
-
-        else {
-            System.out.print("No crash boom :(");
-        }
-
-        SmartDashboard.putBoolean("Collision Detected", collisionDetected);
-        return collisionDetected;
-        
-    }
-
-    public void resetCollisionDetector() {
-        collisionDetected = false;
+    public void angularAccelerationCalculations() {
+        alphaX = (navx.getRawGyroX() * (Math.PI / 180)) / time;
+        alphaY = (navx.getRawGyroY() * (Math.PI / 180)) / time;
     }
 
     public void accelerationDiagnostics() {
-        SmartDashboard.putNumber("X acceleration", currentWorldLinearAccelX);
-        SmartDashboard.putNumber("Y acceleration", currentWorldLinearAccelY);
-        SmartDashboard.putNumber("X Jerk", currentJerkX);
-        SmartDashboard.putNumber("Y Jerk", currentJerkY);
+        angularAccelerationCalculations();
+        SmartDashboard.putNumber("ForwardBackward acceleration", alphaY);
+        SmartDashboard.putNumber("LeftRight acceleration", alphaX);
     }
 }
