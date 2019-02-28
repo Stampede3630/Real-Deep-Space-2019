616002c40c5aaa67b42afc6f7943077c7537bce8
camclarke64 <camsyta81600@gmail.com>
2019-02-28 17:09:22 -0600

    MOAR TESTS

--- DeepSpace/src/main/java/frc/robot/NavX.java
+++ DeepSpace/src/main/java/frc/robot/NavX.java
@@ -1,5 +1,30 @@
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class NavX {
-    
+
+    AHRS navxAhrs = new AHRS(SPI.Port.kMXP);
+
+    double alphaX = navxAhrs.getWorldLinearAccelX();
+    double alphaY = navxAhrs.getWorldLinearAccelY();
+    double jerkX;
+    double jerkY;
+    double time = 0.02;
+    double jerkThreshold = 0.4;
+
+    boolean hasCrashed = false;
+
+    public void crashTest() {
+        jerkX = alphaX / time;
+        jerkY = alphaY / time;
+
+        if ((Math.abs(jerkX) > jerkThreshold || Math.abs(jerkY) > jerkThreshold)) {
+            hasCrashed = true;
+            SmartDashboard.putBoolean("Crash detected", hasCrashed);
+        }
+    }
 }
