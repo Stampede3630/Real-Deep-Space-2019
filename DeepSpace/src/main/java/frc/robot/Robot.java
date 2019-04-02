ada69d5750e9387fd970a8c17a1ac6b3a762bdc5
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2019-04-02 16:07:17 -0500

    new start

--- DeepSpace/src/main/java/frc/robot/Robot.java
+++ DeepSpace/src/main/java/frc/robot/Robot.java
@@ -7,34 +7,18 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends TimedRobot 
 {
  
-
-  public static DriveTrain driveTrain;
-  public static Manipulator manipulator;
-  public Diagnostics diagnostics;
-  public static Choosers choosers;
-  public static SendableChooser manipulatorChooser;
-  public static PathChooser pathChooser = new PathChooser();
-  public static GrassHopper magicWillHappen = new GrassHopper();
+  
 
   @Override
   public void robotInit() 
   {
-    driveTrain = new DriveTrain();
-    manipulator = new Manipulator();
-    diagnostics = new Diagnostics();
-    choosers = new Choosers(driveTrain, manipulator);
-    manipulatorChooser = new SendableChooser();
-    manipulatorChooser.addDefault("Hatch Forward", "Hatch");
-    manipulatorChooser.addObject("Ball Forward", "Ball");
-    SmartDashboard.putData("Forward Chooser", manipulatorChooser);
-    SmartDashboard.putString("Path Selected", "");
+    
   }
   
   @Override
   public void robotPeriodic() 
   {
-    diagnostics.toSmartDashboard();
-    pathChooser.stringToPath(SmartDashboard.getString("Path Selected", ""));
+    
   }
 
   
@@ -60,32 +44,13 @@ public class Robot extends TimedRobot
   @Override
   public void teleopPeriodic() 
   {
-    choosers.setDriveMode();
-    //choosers.automatedTurnToAngle();
-    choosers.setManipulatorMode();
-
-//    choosers.chooserAngle(pathChooser.angle);
-
-//    choosers.angleSwitch();
-
-//    choosers.setAction();
-
-    driveTrain.drive();
-
-    manipulator.manipulatorPeriodic();
-
-    choosers.letterButtons();
-
-//    diagnostics.toSmartDashboard();
+    
   }
 
  
   @Override
   public void testPeriodic() 
   {
-   magicWillHappen.hopUpTest();
-
-
-
+   
   }
 }
